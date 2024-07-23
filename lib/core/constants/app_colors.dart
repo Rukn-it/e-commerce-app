914c35cc960ee67646a35cb9ecdad726bceca88d
HebaOsama15 <144209328+HebaOsama15@users.noreply.github.com>
2024-07-24 01:28:33 +0400

    littil editing in  constant/app_color file and change font type in constant/text_style

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart' show Color;
 
-const Color primaryColor = Color(0xFF704F38);
-const Color secondColor = Color(0xFFF7F2ED);
-const Color scaffoldColor = Color(0xFFF2EFED);
+
+const Color primaryColor = Color(0xFF704f38);
+const Color secondColor = Color(0xFFeee5db); 
+const Color thirdColor = Color(0xFFf7f2ed); 
+const Color scaffoldColor = Color(0xFFF3EDE8); //هذا عشان لون الصفحة كامل
 const Color backgroundColor = Color(0xFFF1F1F1);
 
