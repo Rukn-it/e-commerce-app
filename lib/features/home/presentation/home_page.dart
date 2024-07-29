4a18218eb0a2aad6acecb2448ab1ea3341dce476
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2024-07-29 17:07:57 +0300

    Update home_page.dart
    solve the overflow error

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -15,47 +15,54 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
-          child: Stack(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //Header SECTION that shows user's location and notification
-                const HomeTitle(),
-                const HomeHeader(),
-                const SizedBox(height: 10),
-                //Search Bar SECTION with filters
-                const HomeSearchBar(),
-                const SizedBox(height: 20),
-                //Sales and ads slider SECTION
-                const SaleAdsSlider(),
-                const SizedBox(height: 10),
-                //Categories SECTION: shows the title and a list of all categories
-                const CategoryHeader(),
-                const CategoryListView(),
-                const SizedBox(height: 20),
-                //Flash Sale SECTION : shows the sale on categorized products
-                const FlashSaleHeader(),
-                //left time on the current sale
-                const SizedBox(height: 20),
-                const SaleCategoryListView(),
-                // products that have sale on category
-                const SizedBox(height: 20),
-                FlashProductsView(),
-                // current category products
-              ],
-            ),
-            //Floating Navigation bar SECTION
-            const Positioned.fill(
-              bottom: 0,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: NavBar(),
+          padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 5),
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child:       Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Header SECTION that shows user's location and notification
+                    HomeTitle(),
+                    HomeHeader(),
+                    SizedBox(height: 10),
+                    //Search Bar SECTION with filters
+                    HomeSearchBar(),
+                    SizedBox(height: 20),
+                    //Sales and ads slider SECTION
+                    SaleAdsSlider(),
+                    SizedBox(height: 10),
+                    //Categories SECTION: shows the title and a list of all categories
+                    CategoryHeader(),
+                    CategoryListView(),
+                    SizedBox(height: 20),
+                    //Flash Sale SECTION : shows the sale on categorized products
+                    FlashSaleHeader(),
+                    //left time on the current sale
+                    SizedBox(height: 20),
+                    SaleCategoryListView(),
+                    // products that have sale on category
+                    SizedBox(height: 20),
+
+                  ],
+                ),
               ),
-            )
-          ]),
-        ),
+              //all products based on selected category on sale
+              FlashProductsView(),
+              //space to make sure all items showed up
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Container(
+                  color: Colors.white,
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
+        )
       ),
+      // Navigation bar SECTION
+      bottomNavigationBar: const NavBar(),
     );
   }
 }
